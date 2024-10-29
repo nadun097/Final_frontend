8123f7e3a940786e8373ab2ff37d1bd95aca5242
Nadun Anjana <nadunanjana31@gmail.com>
2024-10-29 23:27:59 +0530

    003

--- kdj2-app/src/App.js
+++ kdj2-app/src/App.js
@@ -1,13 +1,13 @@
 // src/App.js
 import React from "react";
 import "./App.css";
-import Header from "./header.js";
+import header from "./header.js";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <h1>This is my first project</h1>
+      <header />
     </div>
   );
 }
--- kdj2-app/src/header.js
+++ kdj2-app/src/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header() {
+export default function header() {
   return (
     <div>
       <h2>react</h2>
