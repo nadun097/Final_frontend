ac73f62ee20a49781388facd2d81724129347ec0
Nadun Anjana <nadunanjana31@gmail.com>
2024-11-06 23:43:51 +0530

    import router

--- kdj2-app/src/App.js
+++ kdj2-app/src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/Header.js";
+import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
 
 function App() {
   return (
