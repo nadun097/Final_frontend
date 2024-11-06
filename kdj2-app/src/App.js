c2313ba01e6f024dbb783d68d1961ba05738dc09
Nadun Anjana <nadunanjana31@gmail.com>
2024-11-06 23:52:31 +0530

    set TrInterface.js for main page

--- kdj2-app/src/App.js
+++ kdj2-app/src/App.js
@@ -2,13 +2,22 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/Header.js";
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/">
+            <div>
+              {/* Your main interface component or content goes here */}
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
