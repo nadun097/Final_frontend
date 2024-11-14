69b5c877d2b11c7325005dde3ffc138ad9470e96
Nadun Anjana <nadunanjana31@gmail.com>
2024-11-14 19:43:11 +0530

    002

--- AMC/src/App.js
+++ AMC/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 import Header from "./component/Header.js";
 import Report from "./NavPages/Report.js";
-import TrInterface from "./NavPages/TrInterface.js";
+import KanbanBoardList from "./NavPages/KanbanBoardList.js";
 import Home from "./NavPages/Home.js";
 import Invoice from "./NavPages/Invoice.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/project" element={<TrInterface />} />
+          <Route path="/project" element={<KanbanBoardList />} />
           <Route path="/invoice" element={<Invoice />} />
           <Route path="/Report" element={<Report />} />
           
