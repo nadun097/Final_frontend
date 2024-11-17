215b33c97b077b62b74a705e6b9dd7f5ab5ffcb3
Fahma <fahmafarick@gmail.com>
2024-11-17 11:23:28 +0530

    reportsNavbar

--- AMC/src/NavPages/Report.js
+++ AMC/src/NavPages/Report.js
@@ -1,9 +1,18 @@
 import React from "react";
+import './Report.css';
 
-export default function Report() {
+ function Report() {
   return (
-    <div>
-      <h4>Report</h4>
-    </div>
+    <nav className="navbar">
+    <ul className="navbar-list">
+      <li><a href="/fullAmc">Full AMC</a></li>
+      <li><a href="/amcPayment">AMC PAYMENT</a></li>
+      <li><a href="/clientWiseAmc">CLIENT WISE AMC</a></li>
+      <li><a href="/amcClientDetails">AMC CLIENT DETAILS</a></li>
+      <li><a href="/renewalAmcs">RENEWAL AMCs</a></li>
+      <li><a href="/scheduledAmc">SCHEDULED AMC</a></li>
+    </ul>
+  </nav>
   );
 }
+export default Report;
